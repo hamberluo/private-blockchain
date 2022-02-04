3eed27ad57f7451f8e010cd429f737738d78c770
hamber.luo <hamber.luo@yijinin.com>
2022-02-05 00:12:14 +0800

    feat: implement validate function

--- src/block.js
+++ src/block.js
@@ -9,7 +9,9 @@
  *  run asynchronous.
  */
 
+import 'body-parser';
 import SHA256 from 'crypto-js';
+import e from 'express';
 import hex2ascii from 'hex2ascii';
 
 class Block {
@@ -39,13 +41,13 @@ class Block {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
+            let oldHash = self.hash;
 
             // Recalculate the hash of the Block
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
+            let newHash = SHA256(JSON.stringify(this)).toString();
 
-            // Returning the Block is valid
-
+            // Returning if the Block is valid
+            resolve(oldHash === newHash);
         });
     }
 
